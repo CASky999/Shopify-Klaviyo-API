a366767304d93213e183df3151b4c4e8af7bc937
CASky999,user.email <bearpig0205@outlook.com,user.password>
2024-08-06 05:46:05 +0900

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
